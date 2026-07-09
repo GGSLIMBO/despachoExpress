2cae47981e04e6f5b846ef352cb7b71d91116e28
GGSLIMBO <87579708+GGSLIMBO@users.noreply.github.com>
2026-07-09 20:01:28 -0300

    Fix 'nordelta' value in zonas object
    Updated the 'zonas' object to change the value for 'nordelta' from 'lejanas' to 'Zonas Lejanas'.

--- data/zonas.js
+++ data/zonas.js
@@ -2,7 +2,7 @@ export const zonas = {
   "san isidro": "GBA 1", "vicente lopez": "GBA 1", "san martin": "GBA 1", "tres de febrero": "GBA 1",
   "hurlingham": "GBA 1", "moron": "GBA 1", "ituzaingo": "GBA 1", "la matanza norte": "GBA 1",
   "lomas de zamora": "GBA 1", "lanus": "GBA 1", "avellaneda": "GBA 1",
-  "tigre": "GBA 2", "san fernando": "GBA 2", "malvinas argentinas": "GBA 2", "jose c paz": "GBA 2", "jose c. paz": "GBA 2", "josé c. paz": "GBA 2", "jose carlos paz": "GBA 2", "josé carlos paz": "GBA 2", "nordelta": "lejanas",
+  "tigre": "GBA 2", "san fernando": "GBA 2", "malvinas argentinas": "GBA 2", "jose c paz": "GBA 2", "jose c. paz": "GBA 2", "josé c. paz": "GBA 2", "jose carlos paz": "GBA 2", "josé carlos paz": "GBA 2", "nordelta": "Zonas Lejanas",
   "san miguel": "GBA 2", "moreno": "GBA 2", "merlo": "GBA 2", "la matanza sur": "GBA 2",
   "ezeiza": "GBA 2", "esteban echeverria": "GBA 2", "almirante brown": "GBA 2", "quilmes": "GBA 2",
   "florencio varela": "GBA 2", "berazategui": "GBA 2",
